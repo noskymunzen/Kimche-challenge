460c533e705f8087573b73507d94f939bb2dd297
noskymunzen <munzenmayer@outlook.com>
2022-05-16 22:07:17 -0400

    style: adds all elements of the Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,30 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Badge } from '@chakra-ui/react';
 
 const Card = (props) => {
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <Text>{props.name}</Text>
+    <Box
+      mb="3"
+      maxW="sm"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      bg="gray.100"
+    >
+      <Box my="2" mx="2">
+        <Text as="b" fontSize="18px">
+          {props.emoji} {props.name}
+        </Text>
+        <Box display="flex" alignItems="baseline">
+          <Text mr="2">Capital: </Text>
+          <Text>{props.capital}</Text>
+        </Box>
+        <Text>Languages:</Text>
+        {props.languages.map((language) => (
+          <Badge mr="2" colorScheme="purple">
+            {language.name}
+          </Badge>
+        ))}
+      </Box>
     </Box>
   );
 };
